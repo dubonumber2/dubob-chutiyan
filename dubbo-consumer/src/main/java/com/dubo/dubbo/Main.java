e0cd959d0d2052b43d83636f44e42d119c4c2dfe
杜波 <dubo@bjksrs.com>
2018-01-30 12:09:37 +0800

    分布式集群

--- dubbo-consumer/src/main/java/com/dubo/dubbo/Main.java
+++ dubbo-consumer/src/main/java/com/dubo/dubbo/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("classpath:spring-mvc.xml");
         UserService userService=(UserService) context.getBean("userService");
-        String a=userService.sayHello("杜波大傻逼");
+        String a=userService.sayHello("落霞与孤鹜齐飞，秋水共长天一色");
         System.out.println(a);
     }
 }
